1a3f96f551c6deda37dd3d5d7386c4bd31bc95d2
aisco <aisling.connaughton@icloud.com>
2021-03-16 13:34:03 +0000

    react todo list application

---
+++ src/AddTodo.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+
+    state = {
+        content: ''
+    }
+
+    // handler functions
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+
+    handleSumbit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSumbit}>
+                    <label>Add new todo:</label>
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo;
